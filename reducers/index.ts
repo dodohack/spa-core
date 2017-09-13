e746356ac7e7a25c3af174c88c3753e8c91d3fd6
Aries Wu <aries@bangli.uk>
2017-09-13 23:22:49 +0100

    Init reducers/index.ts

---
+++ reducers/index.ts
@@ -0,0 +1,12 @@
+import { ActionReducerMap } from '@ngrx/store';
+
+
+// Debug tools
+import { storeLogger } from 'ngrx-store-logger';
+import { storeFreeze } from 'ngrx-store-freeze';
+
+import * as fromEntities from './entities';
+
+export interface State {
+    topics:
+}
