3445a3fa45abdbd0a9a1ffda0873f2043b6cb618
Aries Wu <aries@bangli.uk>
2017-09-14 22:34:21 +0100

    More draft

--- reducers/index.ts
+++ reducers/index.ts
@@ -1,12 +1,29 @@
-import { ActionReducerMap } from '@ngrx/store';
+/**
+ * Ref: https://github.com/ngrx/platform/blob/master/example-app/app/reducers/index.ts
+ */
 
+import {
+    ActionReducerMap,
+    createSelector,
+    createFeatureSelector,
+    ActionReducer,
+    MetaReducer } from '@ngrx/store';
 
 // Debug tools
 import { storeLogger } from 'ngrx-store-logger';
 import { storeFreeze } from 'ngrx-store-freeze';
 
 import * as fromEntities from './entities';
+import {
+    topicReducer,
+    offerReducer
+} from './entities'
 
 export interface State {
-    topics:
+    //topics:
+}
+
+export const reducers: ActionReducerMap<State> = {
+    topics: topicReducer,
+    offers: offerReducer,
 }
